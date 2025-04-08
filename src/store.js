b61a0c8fe1b186e5e6369e840c737e9631cb415e
Deniz <auo@tuta.io>
2025-04-08 10:02:24 -0400

    Updated until Task 5

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-
+import avReducer from './avSlice';
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
   },
 });
